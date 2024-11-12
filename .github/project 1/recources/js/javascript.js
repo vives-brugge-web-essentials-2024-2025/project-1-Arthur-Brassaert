378ee2f359348689e959955d2d59ef1f05d90414
Arthur Brassaert <abrassaert@gmail.com>
2024-11-12 10:23:46 +0100

    basis gemaakt, divs in contactformulier moeten nog naast elkaar

---
+++ .github/project 1/recources/js/javascript.js
@@ -0,0 +1,10 @@
+// Toon de knop bij scrollen
+window.addEventListener("scroll", function() {
+    const backToTopButton = document.querySelector(".back-to-top");
+    if (window.scrollY > 200) { // Toon de knop na 200px scrollen
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+  
